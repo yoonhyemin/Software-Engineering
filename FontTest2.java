043d96a723ad3f0be7abf58486269a6c90c5c2bd
lee hwa young <ownersky@naver.com>
2016-12-07 16:21:17 +0900

    4th version

---
+++ FontTest2.java
@@ -0,0 +1,21 @@
+
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: FontTest2
+ *
+ */
+@Entity
+
+public class FontTest2 implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public FontTest2() {
+		super();
+	}
+   
+}
